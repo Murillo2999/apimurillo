98e08f72091738788a9229ca2ef6c4caa3987a48
Etec Prof. Horácio Augusto da Silveira <murillo25.martins@hotmail.com>
2024-11-13 22:47:06 -0300

    first commit

---
+++ Models/Enum/Unidades.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MurilloApi.Models
+{
+    public class Unidades
+    {
+        
+    }
+}
